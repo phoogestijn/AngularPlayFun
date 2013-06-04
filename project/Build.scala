494fbe8452f7738237e43b9cc7db85f3af755872
Joost de Vries <joost.git@ziener.org>
2013-06-04 11:56:55 +0200

    use webjars for bootstrap

--- project/Build.scala
+++ project/Build.scala
@@ -10,11 +10,11 @@ object ApplicationBuild extends Build {
   val appDependencies = Seq(
     "org.webjars" % "angularjs" % "1.1.5-1",
     "org.webjars" % "requirejs" % "2.1.1",
+    "org.webjars" % "bootstrap" % "2.3.2",
     "org.webjars" % "webjars-play" % "2.1.0-1",
     "org.webjars" % "jasmine" % "1.3.1")
 
-  val main = play.Project(
-    appName, appVersion, appDependencies).settings(
+  val main = play.Project(  appName, appVersion, appDependencies).settings(
       requireJs += "main.js",
       requireJsShim += "main.js")
 
